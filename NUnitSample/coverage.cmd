c5ad75c269ddc5b795aa9d391f57f33458b84281
crvidya <crvidya@ymail.com>
2013-09-26 20:15:11 +0530

    Update coverage.cmd

--- NUnitSample/coverage.cmd
+++ NUnitSample/coverage.cmd
@@ -1,1 +1,1 @@
-packages\OpenCover.4.5.1403\OpenCover.Console.exe -register:user -target:"nunitwrapper.cmd" -filter:"+[*]*" -output:..\qhode\codecoverage\report.xml -log:All
+packages\OpenCover.4.5.1403\OpenCover.Console.exe -register:user -target:"nunitwrapper.cmd" -filter:"+[*]*" -output:..\shippable\codecoverage\report.xml -log:All
